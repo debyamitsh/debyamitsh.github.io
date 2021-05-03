f4b1422d746fa1bdcfdc3192c765d003ef487c24
Deborah Yambenu <deboryambenu@gmail.com>
2021-05-03 07:47:50 +0200

    add link to my personal website

--- js/main.js
+++ js/main.js
@@ -1,4 +1,75 @@
 /**
+ * nav menu
+ */
+(() =>{
+    const hambugerBtn = document.querySelector(".hamburger-btn"),
+        navMenu = document.querySelector(".nav-menu"),
+        closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+    hambugerBtn.addEventListener("click",showNavMenu);
+    closeNavBtn.addEventListener("click",hideNavMenu);
+
+    function showNavMenu(){
+        navMenu.classList.add("open");
+        bodyScrollingToggle();
+    }
+
+    function hideNavMenu(){
+        navMenu.classList.remove("open");
+        fadeOutEffect();
+        bodyScrollingToggle();
+    }
+    
+    function fadeOutEffect() {
+        document.querySelector(".fade-out-effect").classList.add("active");
+        setTimeout(() =>{
+            document.querySelector(".fade-out-effect").classList.remove("active");
+        }, 300)
+    }
+    // attach and event handler to document
+    document.addEventListener("click", (event) =>{
+        if (event.target.classList.contains("link-item")){
+            // make sure event.target.hash has a value before overridding default behavior
+            if(event.target.hash !==""){
+                // prevent default anchor click behavior
+                event.preventDefault();
+                const hash = event.target.hash;
+                //deactivate existing active section
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+                //activate new section
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+                // deactive existing active navigation menu "link-item"
+                navMenu.querySelector(".active").classList.add("outer-shadow","hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active","inner-shadow");
+                // if clicked link-item is contained withing the navigation menu
+                if (navMenu.classList.contains("open")){
+                    //activate new navigation menu 'link-item'
+                    event.target.classList.add("active","inner-shadow");
+                    event.target.classList.remove("outer-shadow","hover-in-shadow");
+                    //hide navigation menu
+                    hideNavMenu();
+                } else {
+                    let navItems = navMenu.querySelectorAll(".link-item");
+                    navItems.forEach((item) =>{
+                        if (hash === item.hash){
+                            console.log(item);
+                            //activate new navigation menu link-item
+                            item.classList.add("active","inner-shadow");
+                            item.classList.remove("outer-shadow","hover-in-shadow");
+                        }
+                    })
+                    fadeOutEffect();
+                }
+
+            }
+        }
+    })
+
+})();
+
+/**
  * about section tabs
  */
 (() => {
@@ -229,5 +300,10 @@ function bodyScrollingToggle() {
 * hide all sections except active
 * */
 (() =>{
-    console.log("hello");
+    const sections = document.querySelectorAll(".section");
+    sections.forEach((section) =>{
+        if(!section.classList.contains("active")){
+            section.classList.add("hide");
+        }
+    })
 })();
